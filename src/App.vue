bb15e3f76e99fed10b5c25f632c757ad1782311d
Miriam Bah <service.miba@proton.me>
2024-01-29 18:26:19 +0100

    feat: create structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import AmountFilter from '@/components/AmountFilter.vue';
+import TransactionsOverview from '@/components/TransactionsOverview.vue';
+import CardSelection from '@/components/CardSelection.vue';
 </script>
 
 <template>
+  <header>
+    <h1>Transaction Overview</h1>
+    <p>Choose the card for which you want to see your transactions:</p>
+    <CardSelection />
+  </header>
+  <main>
+    <AmountFilter />
+    <TransactionsOverview />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/CardSelection.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Cards Overview
+  </div>
+</template>
